d3c2428d55e936a3edd7b402825da190da7c7dca
chronicallyunfunny <48951297+chronicallyunfunny@users.noreply.github.com>
2021-05-12 17:51:39 +0800

    Delete constants.rs

--- src/constants.rs
+++
@@ -1,8 +0,0 @@
-pub const CONFIG_PATH: &str = "config.toml";
-pub const YGGDRASIL_ORIGIN_SERVER: &str = "https://authserver.mojang.com";
-pub const MOJANG_API_SERVER: &str = "https://api.mojang.com";
-pub const MINECRAFTSERVICES_API_SERVER: &str = "https://api.minecraftservices.com";
-pub const MS_AUTH_SERVER: &str = "https://login.live.com/oauth20_authorize.srf?client_id=68f2f45b-02e2-4625-8225-25c6fcc25039&response_type=code&redirect_uri=https://buckshotrs.com/auth:1338&scope=XboxLive.signin%20offline_access&state=STORAGE_ID";
-pub const SERVER_RESPONSE_DURATION: i32 = 100;
-pub const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36";
-pub const TEUN_NAMEMC_API: &str = "https://mojang-api.teun.lol";
