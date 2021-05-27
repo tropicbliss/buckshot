dbf1ac47de9a8784c207a9f6ff1c194b2ae62208
Eugene Toh <eugenetoh54@gmail.com>
2021-05-27 10:41:15 +0800

    Send for testing.

--- src/requests.rs
+++ src/requests.rs
@@ -116,6 +116,7 @@ impl Requests {
             let first = v[0]["answer"]["id"].as_u64().unwrap() as u8;
             let second = v[1]["answer"]["id"].as_u64().unwrap() as u8;
             let third = v[2]["answer"]["id"].as_u64().unwrap() as u8;
+            println!("{:?}", [first, second, third]);
             Some([first, second, third])
         }
     }
