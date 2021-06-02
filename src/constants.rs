7e5a877830b9101daa75fc15d89e3b53a2bd57b7
Eugene Toh <eugenetoh54@gmail.com>
2021-06-02 15:34:59 +0800

    E

--- src/constants.rs
+++ src/constants.rs
@@ -4,7 +4,7 @@ pub const MOJANG_API_SERVER: &str = "https://api.mojang.com";
 pub const MINECRAFTSERVICES_API_SERVER: &str = "https://api.minecraftservices.com";
 pub const MS_AUTH_SERVER: &str = "https://login.live.com/oauth20_authorize.srf?client_id=68f2f45b-02e2-4625-8225-25c6fcc25039&response_type=code&redirect_uri=https://auth.buckshotrs.com/auth&scope=XboxLive.signin%20offline_access&state=STORAGE_ID";
 pub const TEUN_NAMEMC_API: &str = "https://mojang-api.teun.lol";
-pub const REGULAR_SNIPE_REQS: u8 = 2;
+pub const REGULAR_SNIPE_REQS: u8 = 3;
 pub const GC_SNIPE_REQS: u8 = 6;
 pub const SERVER_RESPONSE_TIME: u8 = 100;
 pub const BUCKSHOT_API_SERVER: &str = "https://auth.buckshot.com";
