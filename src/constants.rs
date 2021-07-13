5c09b9363bbaa95e246ab954e04ee1fbbf478872
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2021-07-14 02:46:49 +0800

    Update constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,4 @@
-pub const CONFIG_PATH: &str = "config.toml";
+pub const DEFAULT_CONFIG_PATH: &str = "config.toml";
 pub const YGGDRASIL_ORIGIN_SERVER: &str = "https://authserver.mojang.com";
 pub const MOJANG_API_SERVER: &str = "https://api.mojang.com";
 pub const MINECRAFTSERVICES_API_SERVER: &str = "https://api.minecraftservices.com";
