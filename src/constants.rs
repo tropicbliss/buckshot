e6b0ff5ab93e3058da57eec2b36a7ef0fd435e97
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2021-07-13 15:57:09 +0800

    Update constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -6,6 +6,7 @@ pub const NAMEMC_API: &str = "http://drops.peet.ws";
 pub const REGULAR_SNIPE_REQS: u8 = 3;
 pub const GC_SNIPE_REQS: u8 = 6;
 pub const SERVER_RESPONSE_TIME: u8 = 40;
+pub const NAMEMC_USER_AGENT: &str = "PiratSnipe";
 pub const BUCKSHOT_API_SERVER: &str = "https://auth.buckshotrs.com";
-pub const USER_AGENT: &str =
+pub const AUTH_USER_AGENT: &str =
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0";
