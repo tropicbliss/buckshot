ad7ffabaf7fd4cf7e3580dc31c791ef3a2ab4c9f
tropicbliss <tropicbliss@protonmail.com>
2021-08-09 16:37:03 +0800

    E

--- src/sockets.rs
+++ src/sockets.rs
@@ -58,6 +58,7 @@ impl Executor {
         spread_offset: usize,
         snipe_time: DateTime<Utc>,
     ) -> Result<bool> {
+        let mut is_success = false;
         let payload = if self.is_gc {
             let post_body = json!({ "profileName": self.name }).to_string();
             format!("POST /minecraft/profile HTTP/1.1\r\nHost: api.minecraftservices.com\r\nAccept: application/json\r\nAuthorization: Bearer {}\r\n\r\n{}", bearer_token, post_body).into_bytes()
@@ -65,9 +66,6 @@ impl Executor {
             format!("PUT /minecraft/profile/name/{} HTTP/1.1\r\nHost: api.minecraftservices.com\r\nAuthorization: Bearer {}\r\n", self.name, bearer_token).into_bytes()
         };
         let req_count = if self.is_gc { 6 } else { 3 };
-        let mut status_vec = Vec::with_capacity(req_count);
-        let mut handle_vec: Vec<JoinHandle<Result<_, anyhow::Error>>> =
-            Vec::with_capacity(req_count);
         let mut spread = 0;
         let addr = "api.minecraftservices.com:443"
             .to_socket_addrs()?
@@ -80,61 +78,65 @@ impl Executor {
             .add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
         let connector = Arc::new(TlsConnector::from(Arc::new(config)));
         let domain = DNSNameRef::try_from_ascii_str("api.minecraftservices.com")?;
-        for _ in 0..req_count {
-            let connector = Arc::clone(&connector);
-            let data = Arc::clone(&data);
-            let handle = tokio::task::spawn(async move {
-                let mut buf = [0; 12];
-                let snipe_time = snipe_time + Duration::milliseconds(spread);
-                let handshake_time = snipe_time - Duration::seconds(3);
-                let sleep_duration = match (handshake_time - Utc::now()).to_std() {
-                    Ok(x) => x,
-                    Err(_) => std::time::Duration::ZERO,
-                };
-                sleep(sleep_duration).await;
-                let stream = TcpStream::connect(&addr).await?;
-                let mut stream = connector.connect(domain, stream).await?;
-                stream.write_all(&data).await?;
-                let sleep_duration = match (snipe_time - Utc::now()).to_std() {
-                    Ok(x) => x,
-                    Err(_) => std::time::Duration::ZERO,
-                };
-                sleep(sleep_duration).await;
-                stream.write_all(b"\r\n").await?;
-                stream.read_exact(&mut buf).await?;
-                let formatted_resp_time = Utc::now().format("%F %T%.6f");
-                let res = String::from_utf8_lossy(&buf);
-                let status = res[9..].parse::<u16>()?;
-                match status {
-                    200 => {
-                        writeln!(
-                            stdout(),
-                            "[{}] {} @ {}",
-                            Green.paint("success"),
-                            Green.paint("200"),
-                            Cyan.paint(format!("{}", formatted_resp_time))
-                        )?;
-                        Ok(true)
+        let handle_vec: Vec<JoinHandle<Result<_, anyhow::Error>>> = (0..req_count)
+            .map(|_| {
+                let connector = Arc::clone(&connector);
+                let data = Arc::clone(&data);
+                let handle = tokio::task::spawn(async move {
+                    let mut buf = [0; 12];
+                    let snipe_time = snipe_time + Duration::milliseconds(spread);
+                    let handshake_time = snipe_time - Duration::seconds(3);
+                    let sleep_duration = match (handshake_time - Utc::now()).to_std() {
+                        Ok(x) => x,
+                        Err(_) => std::time::Duration::ZERO,
+                    };
+                    sleep(sleep_duration).await;
+                    let stream = TcpStream::connect(&addr).await?;
+                    let mut stream = connector.connect(domain, stream).await?;
+                    stream.write_all(&data).await?;
+                    let sleep_duration = match (snipe_time - Utc::now()).to_std() {
+                        Ok(x) => x,
+                        Err(_) => std::time::Duration::ZERO,
+                    };
+                    sleep(sleep_duration).await;
+                    stream.write_all(b"\r\n").await?;
+                    stream.read_exact(&mut buf).await?;
+                    let formatted_resp_time = Utc::now().format("%F %T%.6f");
+                    let res = String::from_utf8_lossy(&buf);
+                    let status = res[9..].parse::<u16>()?;
+                    match status {
+                        200 => {
+                            writeln!(
+                                stdout(),
+                                "[{}] {} @ {}",
+                                Green.paint("success"),
+                                Green.paint("200"),
+                                Cyan.paint(format!("{}", formatted_resp_time))
+                            )?;
+                            Ok(true)
+                        }
+                        status => {
+                            writeln!(
+                                stdout(),
+                                "[{}] {} @ {}",
+                                Red.paint("fail"),
+                                Red.paint(format!("{}", status)),
+                                Cyan.paint(format!("{}", formatted_resp_time))
+                            )?;
+                            Ok(false)
+                        }
                     }
-                    status => {
-                        writeln!(
-                            stdout(),
-                            "[{}] {} @ {}",
-                            Red.paint("fail"),
-                            Red.paint(format!("{}", status)),
-                            Cyan.paint(format!("{}", formatted_resp_time))
-                        )?;
-                        Ok(false)
-                    }
-                }
-            });
-            spread += spread_offset as i64;
-            handle_vec.push(handle);
-        }
+                });
+                spread += spread_offset as i64;
+                handle
+            })
+            .collect();
         for handle in handle_vec {
             let status = handle.await??;
-            status_vec.push(status);
+            if status {
+                is_success = true;
+            }
         }
-        Ok(status_vec.contains(&true))
+        Ok(is_success)
     }
 }
