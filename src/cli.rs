846e870ce76a67527348c52915b79359a893f482
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2021-09-12 11:29:28 +0800

    Update cli.rs
    Fix the issue of unable to input uppercase in name prompt

--- src/cli.rs
+++ src/cli.rs
@@ -24,6 +24,6 @@ impl Args {
 pub fn get_name_choice() -> Result<String> {
     let name: String = Input::new()
         .with_prompt("What name would you like to snipe")
-        .interact_text()?;
+        .interact()?;
     Ok(name)
 }
