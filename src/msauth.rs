3d2a97c8198c67ed60b4007836287a0d94a7dea9
tropicbliss <tropicbliss@protonmail.com>
2021-10-24 14:20:14 +0800

    E

--- src/msauth.rs
+++ src/msauth.rs
@@ -142,7 +142,7 @@ impl<'a> Auth<'a> {
             .collect();
         Ok(param
             .remove("access_token")
-            .ok_or_else(|| anyhow!("Unable to extract access_token from JSON"))?
+            .ok_or_else(|| anyhow!("Unable to extract access_token from URL"))?
             .to_string())
     }
 
