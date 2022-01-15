bc50f308dde6a779d8f342abbb0f35e10ec8bdc1
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2022-01-15 23:00:44 +0800

    Update constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,5 @@
 use std::time::Duration;
 
 pub const CONFIG_PATH: &str = "config.toml";
-pub const IO_TIMEOUT: Duration = Duration::from_secs(5);
+pub const IO_TIMEOUT: Duration = Duration::from_secs(27);
 pub const BARRIER_THRESHOLD: u32 = 27;
